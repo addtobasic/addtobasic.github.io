6d481f86cbca8d214a75bfd19975db5802c8d6d3
Genshi0916 <genshi0916@gmail.com>
2021-09-04 23:55:12 +0900

    :art: terminal部分のtitle-barの追加

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -4,14 +4,25 @@ const Terminal: FC = () => {
   return (
     <>
       <div className='h-screen pt-20'>
-        <div className='block bg-gray-200 h-6 rounded-t-xl border-b-2 border-gray-200'>
-          <span className='inline-block h-4 w-4 bg-red-400 rounded-full justify-center items-center ml-4' />
-          <span className='inline-block h-4 w-4 bg-yellow-400 rounded-full justify-center items-center mx-2' />
-          <span className='inline-block h-4 w-4 bg-green-400 rounded-full justify-center items-center' />
+        <div className='flex justify-end bg-title-bar h-10 rounded-t-xl border-b-2 border-gray-600'>
+          <span className='inline-block h-5 w-5 bg-title-bar-button rounded-full  mt-2 text-sbase'>
+            <span className='ml-1'>-</span>
+          </span>
+          <span className='inline-block h-5 w-5 bg-title-bar-button rounded-full  mx-2 mt-2 text-base'>
+            <span className='ml-1'>□</span>
+          </span>
+          <span className='inline-block h-5 w-5 bg-red-500 rounded-full mr-2 mt-2 text-base'>
+            <span className='ml-1'>×</span>
+          </span>
         </div>
-        <div className='block bg-gray-800 opacity-90 h-5/6 shadow-2xl rounded-b-xl p-2 overflow-auto md:p-8'>
-          <p>hogehoge</p>
-          <input id='command-area' type='text'/>
+        <div className='block bg-ubuntu-terminal opacity-90 h-5/6 shadow-2xl rounded-b-xl p-2 overflow-auto md:p-8'>
+          <p>genshi@genshi0916 ~ $ </p>
+          <input
+            className='bg-transparent focus-within:outline-none'
+            id='command-area'
+            type='text'
+            autoComplete='off'
+          />
         </div>
       </div>
     </>
