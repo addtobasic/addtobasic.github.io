d3e61025b635a55a1c12866b0a2cb896a539bdd6
Genshi0916 <genshi0916@gmail.com>
2021-09-22 19:05:39 +0900

    :sparkles: 不要な改行の削除とコンポーネントの省略

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import React, { FC, useRef } from 'react';
+import React, { FC } from 'react';
 import Head from 'next/head';
 import Terminal from '../components/Terminal';
 
 const Home: FC = () => {
-
   const anyWhereClick = () => {
     document.getElementById('command-area').focus();
   };
@@ -14,9 +13,8 @@ const Home: FC = () => {
         <title>genshi@genshi0916: ~</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='container mx-auto sm:w-5/6 md:w-4/6 :w-2/4 xl:w-5/12 text-shellGreen text-xl' >
-        <Terminal >
-        </Terminal>
+      <div className='container mx-auto sm:w-5/6 md:w-4/6 :w-2/4 xl:w-5/12 text-shellGreen text-xl'>
+        <Terminal />
       </div>
     </div>
   );
