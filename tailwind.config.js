9b6235f4910e3065c6bce34e1a771d471db600e4
Genshi0916 <genshi0916@gmail.com>
2021-10-13 17:36:26 +0900

    :pencil: purgeの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
