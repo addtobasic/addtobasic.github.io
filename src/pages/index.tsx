cf96c8cfc9243293a5e64d4cb4cc38fddd791965
addtobasic <addtobasic@gmail.com>
2021-10-27 00:42:18 +0900

    :memo: metaタグの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,14 +12,17 @@ const Home: FC = () => {
       <Head>
         <title>genshi@addtobasic: ~</title>
         <link rel='icon' href='/favicon.ico' />
-        <meta property="og:title" content="Genshi's CUI Portfolio" />
-        <meta property="og:description" content="CUI Portfolio like ubuntu terminal." />
-        <meta property="og:url" content="https://addtobasic.github.io/" />
-        <meta property="og:type" content="website" />
-        <meta property="twitter:card" content="summary" />
+        <meta property='og:title' content="Genshi's CUI Portfolio" />
         <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/addtobasic/portfolio/master/assets/genshi.jpg"
+          property='og:description'
+          content='CUI Portfolio like ubuntu terminal.'
+        />
+        <meta property='og:url' content='https://addtobasic.github.io/' />
+        <meta property='og:type' content='website' />
+        <meta property='twitter:card' content='summary' />
+        <meta
+          property='og:image'
+          content='https://raw.githubusercontent.com/addtobasic/portfolio/master/assets/genshi.jpg'
         />
       </Head>
       <div className='container mx-auto sm:w-5/6 md:w-4/6 :w-2/4 xl:w-5/12 text-shellGreen text-xl'>
