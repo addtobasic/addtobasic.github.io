0b5997914326871d2e64de9cc9343739e9d96e10
addtobasic <addtobasic@gmail.com>
2021-11-23 00:32:52 +0900

    :sparkles: utilにwhiteのデータを追加

--- src/util.ts
+++ src/util.ts
@@ -2,9 +2,15 @@ export const HOME_PATH: string = '/home';
 export const GENSHI_PATH: string = '/home/genshi';
 export const PRODUCTS_PATH: string = '/home/genshi/products';
 export const CONTACTS_PATH: string = '/home/genshi/contacts';
+export const WHITE_PATH: string = '/home/genshi/white';
 
 export const LS_HOME_ITEM: string[] = ['genshi'];
-export const LS_GENSHI_ITEM: string[] = ['profile.txt', 'products', 'contacts'];
+export const LS_GENSHI_ITEM: string[] = [
+  'profile.txt',
+  'products',
+  'contacts',
+  'white',
+];
 export const LS_PRODUCTS_ITEM: string[] = [
   'portfolio.txt',
   'cui-portfolio.txt',
@@ -19,3 +25,55 @@ export const LS_CONTACTS_ITEM: string[] = [
   'qiita.txt',
   'gmail.txt',
 ];
+export const LS_WHITE_ITEM: string[] = [
+  'README.md',
+  'main.pyt',
+  'pyproject.toml',
+  'requirements.txt',
+];
+
+export const CAT_FILE_CONTENTS = {
+  'profile.txt': {
+    content: 'Name: Genshi, Age: 19, Likes: Twitter, NovelGame',
+  },
+  'portfolio.txt': {
+    content: 'addtobasic.dev',
+    url: 'https://addtobasic.dev/',
+  },
+  'cui-portfolio.txt': {
+    content: 'CUI Portfolio like ubuntu',
+    url: 'https://github.com/addtobasic/addtobasic.github.io',
+  },
+  'deadline-timer.txt': {
+    content: '名言が表示される邪悪なタイマー',
+    url: 'https://addtobasic.github.io/deadline-timer/',
+  },
+  'download-pixiv-images.txt': {
+    content: 'pixivのブックマークをすべてダウンロードしてくるアプリ',
+    url: 'https://github.com/addtobasic/auto-download-pixiv-bookmark-images',
+  },
+  'twitter.txt': {
+    content: 'Twitter: addtobasic',
+    url: 'https://twitter.com/addtobasic',
+  },
+  'github.txt': {
+    content: 'GitHub: addtobasic',
+    url: 'https://github.com/addtobasic',
+  },
+  'facebook.txt': {
+    content: 'facebook: Genki Kano',
+    url: 'https://www.facebook.com/genki.kano.794',
+  },
+  'instagram.txt': {
+    content: 'instagram: addtobasic',
+    url: 'https://www.instagram.com/addtobasic/',
+  },
+  'qiita.txt': {
+    content: 'Qiita: addtobasic',
+    url: 'https://qiita.com/addtobasic',
+  },
+  'gmail.txt': {
+    content: 'Gmail: addtobasic@gmail.com',
+    url: 'mailto:addtobasic@gmail.com',
+  },
+};
