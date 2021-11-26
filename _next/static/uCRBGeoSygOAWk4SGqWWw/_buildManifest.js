658a07450d02bc8ed34826234eb025fc75a55e2d
addtobasic <addtobasic@users.noreply.github.com>
2021-11-26 08:02:54 +0000

    deploy: 0594682776129cd2b96565b3e49d8419456878ff

--- _next/static/uCRBGeoSygOAWk4SGqWWw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c4f2e3f7b04959de64d6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
