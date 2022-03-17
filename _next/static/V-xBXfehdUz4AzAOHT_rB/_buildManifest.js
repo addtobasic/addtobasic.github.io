89bed3ba098f9fd51a5de6406d1b583d8162d098
addtobasic <addtobasic@users.noreply.github.com>
2022-03-17 10:16:11 +0000

    deploy: 94bf6a648a1e34b9dc0faf32d7513c9ea92561d7

---
+++ _next/static/V-xBXfehdUz4AzAOHT_rB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3a9d07e21f08ff33.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
